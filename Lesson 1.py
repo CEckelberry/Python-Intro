7f36c0f4ac1263d3c756adbfa2542c13c93441b4
Cole Eckelberry <cole.eckelberry@gmail.com>
2018-02-05 22:39:24 -0500

    Stopping Point 2

--- Lesson 1.py
+++ Lesson 1.py
@@ -1,4 +1,63 @@
+
+######Part 2 Arithmetic######
+
 print(3 +1)
 
 print("Hello World!")
+
+print ((1 + 2 + 3) *3)
+
+print(3**2)
+
+print(9 % 2)
+
+
+######Part 5 Variables 1######
+
+manila_pop = 1780148
+
+print(manila_pop)
+
+manila_area = 16.56
+
+manila_pop_density = manila_pop / manila_area
+
+print(manila_pop_density)
+
+# The current volume of a water reservoir (in cubic metres)
+reservoir_volume = 4.445e8
+# The amount of rainfall from a storm (in cubic metres)
+rainfall = 5e6
+
+# decrease the rainfall variable by 10% to account for runoff
+rainfall = rainfall - (rainfall*.10)
+# add the rainfall variable to the reservoir_volume variable
+reservoir_volume += rainfall
+# increase reservoir_volume by 5% to account for stormwater that flows
+reservoir_volume = reservoir_volume + (reservoir_volume * .05)
+# into the reservoir in the days following the storm
+# decrease reservoir_volume by 5% to account for evaporation
+reservoir_volume = reservoir_volume - (reservoir_volume * .05)
+# subtract 2.5e5 cubic metres from reservoir_volume to account for water
+# that's piped to arid regions.
+reservoir_volume -= 2.5e5
+# print the new value of the reservoir_volume variable
+print(reservoir_volume)
+
+
+
+######Part 7 Operators######
+
+
+
+
+
+
+
+
+
+
+
+
+
 
